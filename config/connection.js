72b04e6ef4fa58bea48fc386e1228b900d42f52e
Lisa Walsh <lwalshphotography@yahoo.com>
2023-01-28 14:47:46 -0500

    update connection and server files

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/socialDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/departmentsDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-
+// Export connection 
 module.exports = mongoose.connection;
+
+// const { connect, connection } = require('mongoose');
+
+// const connectionString =
+//     process.env.MONGODB_URI || 'mongodb://localhost/socialDB';
+    
+// });
+
+// connect(connectionString, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+
+// module.exports = connection;
