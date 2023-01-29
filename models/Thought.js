95ccf50fbb5a263f4d8966488b855cf84c2d2d51
Lisa Walsh <lwalshphotography@yahoo.com>
2023-01-29 09:11:13 -0500

    added thoughts schema

--- models/Thought.js
+++ models/Thought.js
@@ -32,3 +32,43 @@ const ReactionSchema = new Schema(
         id: false
     }
 );
+
+// thought schema
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// friend count virtual
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+// create the Thought model using the ThoughtSchema
+const Thought = model('Thought', ThoughtSchema);
+
+// export the Thought model
+module.exports = Thought;
